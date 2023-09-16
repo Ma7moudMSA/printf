6446cd01362f334b0ad2aeee9d505f525c97660c
Mahmoud <mahmoud.m.subhy@gmail.com>
2023-09-16 10:43:59 -0700

    Fixing error in Betty-style

--- printf.c
+++ printf.c
@@ -1,54 +1,86 @@
 #include "main.h"
 
 /**
- * _printf - printf function
- * 
- * @format: format.
- * 
- *  Return: Printed chars.
+ * print_char - Prints a character
+ * @args: List of arguments
+ * @count: Pointer to the count of characters
  */
-/*
-*by mahmoud medhat and moumen ibrahim
-*/
+void print_char(va_list args, int *count)
+{
+	int c = va_arg(args, int);
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    int count = 0;
-    for (const char *traverse = format; *traverse != '\0'; traverse++) {
-        while (*traverse != '%' && *traverse != '\0') {
-            putchar(*traverse);
-            traverse++;
-            count++;
-        }
-
-        if (*traverse == '\0') break;
-
-        switch (*++traverse) {
-            case 'c': {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-                break;
-            }
-            case 's': {
-                char *s = va_arg(args, char *);
-                for (; *s; s++) {
-                    putchar(*s);
-                    count++;
-                }
-                break;
-            }
-            case '%': {
-                putchar('%');
-                count++;
-                break;
-            }
-        }
-    }
-
-    va_end(args);
-    return count;
+	putchar(c);
+	(*count)++;
 }
 
+/**
+ * print_string - Prints a string
+ * @args: List of arguments
+ * @count: Pointer to the count of characters
+ */
+void print_string(va_list args, int *count)
+{
+	char *s = va_arg(args, char *);
+
+	for (; *s; s++)
+	{
+		putchar(*s);
+		(*count)++;
+	}
+}
+
+/**
+ * print_percent - Prints a percent character
+ * @count: Pointer to the count of characters
+ */
+void print_percent(int *count)
+{
+	putchar('%');
+	(*count)++;
+}
+
+/**
+ * _printf - printf function
+ * @format: format.
+ *
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	const char *traverse;
+
+	va_start(args, format);
+
+	for (traverse = format; *traverse != '\0'; traverse++)
+	{
+		while (*traverse != '%' && *traverse != '\0')
+		{
+			putchar(*traverse);
+			traverse++;
+			count++;
+		}
+
+		if (*traverse == '\0')
+			break;
+
+		switch (*++traverse)
+		{
+		case 'c':
+			print_char(args, &count);
+			break;
+		case 's':
+			print_string(args, &count);
+			break;
+		case '%':
+			print_percent(&count);
+			break;
+		default:
+			break;
+		}
+	}
+
+	va_end(args);
+	return (count);
+}
