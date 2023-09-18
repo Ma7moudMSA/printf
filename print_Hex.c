03e6513c6828dda01cd25624f8a13452722e5628
Mahmoud <mahmoud.m.subhy@gmail.com>
2023-09-18 03:36:45 -0700

    Print Hex

---
+++ print_Hex.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_Hex - prints the Hexa representation of a number
+ * @number: the number to be printed
+ * @count: a pointer to the count of characters printed
+ */
+void print_Hex(unsigned int number, int *count)
+{
+	char hexDigits[] = "0123456789ABCDEF";
+	unsigned int mask = 0xF0000000;
+
+	while (mask > 0)
+	{
+		unsigned int nibble = (number & mask) >> ((sizeof(unsigned int) * 8) - 4);
+		(*count)++;
+		putchar(hexDigits[nibble]);
+		mask >>= 4;
+	}
+}
